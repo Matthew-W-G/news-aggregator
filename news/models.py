985f874eecee834424ed62b43ee22883ecd2f623
Matthew-W-G <68626329+Matthew-W-G@users.noreply.github.com>
2022-06-12 22:43:41 -0400

    initial changes

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ class NewsPiece(models.Model):
     title = models.CharField(max_length=100)
     blurb = models.TextField()
     source = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
