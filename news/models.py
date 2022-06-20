3894d094cc2ae19270e4f855020e2f088f52e1db
Matthew-W-G <68626329+Matthew-W-G@users.noreply.github.com>
2022-06-20 11:01:15 -0400

    Added code to eliminate duplicates and remove out of date articles

--- news/models.py
+++ news/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class HeadlineBasket(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 class NewsPiece(models.Model):
     title = models.CharField(max_length=100)
     blurb = models.TextField()
     source = models.CharField(max_length=100)
     url = models.CharField(max_length=100, default="")
     date = models.CharField(max_length=100, default="")
-    headline = models.ForeignKey(HeadlineBasket, on_delete=models.CASCADE,default='', null=True, blank=True)
+    headline = models.ForeignKey(HeadlineBasket, on_delete=models.CASCADE,default='')
 
 
     def __str__(self):
--- news/views.py
+++ news/views.py
@@ -4,28 +4,49 @@ from .models import NewsPiece
 from .models import HeadlineBasket
 import json, requests
 from newsapi.newsapi_client import NewsApiClient
+from datetime import datetime, timezone
+from dateutil import parser
+
+newsapi = NewsApiClient(api_key='92c75fd3c7704a00b972bbc242b9907c')
 
 class NewsPieceListView(ListView):
-    newsapi = NewsApiClient(api_key='92c75fd3c7704a00b972bbc242b9907c')
-    top_headlines = newsapi.get_top_headlines(
-                                          language='en',
-                                          country='us')
+    top_headlines = newsapi.get_top_headlines(language='en', country='us')
 
 
-    headline_articles = HeadlineBasket(title="Headlines").save()
+    top_headline_articles = HeadlineBasket(title="Top Headlines")
+    top_headline_articles.save()
 
 
     for ar in top_headlines.get('articles'):
-        NewsPiece(title=ar.get('title'),
-                blurb=ar.get('description'),
-                source=ar.get('source').get('name'),
-                url=ar.get('url'),
-                date=ar.get('publishedAt'),
-                headline=headline_articles
-                ).save()
+        if ar.get('title') not in str(top_headline_articles.newspiece_set.all()):
+            NewsPiece(title=ar.get('title'),
+                    blurb=ar.get('description'),
+                    source=ar.get('source').get('name'),
+                    url=ar.get('url'),
+                    date=ar.get('publishedAt'),
+                    headline=top_headline_articles
+                    ).save()
 
+
+    for ar in top_headline_articles.newspiece_set.all():
+        datetime_object = parser.parse(ar.date)
+        time_since_publish = datetime.now(timezone.utc) - datetime_object
+        if time_since_publish.days >= 1:
+            ar.delete()
+
+
+    queryset = top_headline_articles.newspiece_set.all()
     model = NewsPiece
     context_object_name = 'NewsPiece'
+
+
+
+class EconomyListView(ListView):
+    model = NewsPiece
+
+
+class PoliticsListView(ListView):
+    model = NewsPiece
 
 
 
